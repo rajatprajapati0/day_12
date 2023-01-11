f296f51ca0a22747ad4fbc2c7cf0fb379a8f0279
rajatprajapati0 <121918730+rajatprajapati0@users.noreply.github.com>
2023-01-11 11:59:14 +0530

    added: program for finding prime no.

---
+++ perfectNo2.sh
@@ -0,0 +1,20 @@
+# !/bin/bash -x
+
+echo Enter a number
+read no
+i=1
+ans=0
+while [ $i -le $(($no/2)) ]
+   do
+       if [  $(($no%$i)) -eq 0 ]
+       then
+          ans=$(($ans + $i))
+       fi
+       i=$(($i + 1))
+done
+if [ $no -eq $ans ]
+then
+echo "$no is perfect"
+else
+echo "$no is not perfect"
+fi
